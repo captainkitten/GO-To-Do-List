d48c65c90044cec599a53133ffee41c93e857864
captainkitten <chungb567@gmail.com>
2014-12-05 21:20:11 +0000

    Create main.go

---
+++ main.go
@@ -0,0 +1,11 @@
+// A simple text output to do list annotated like a BOSS!
+// Fork this and have a play about with your routine
+
+import "fmt"
+
+//Where all the magic happens
+func main() {
+  fmt.println("Hello Programmer!")
+  fmt.println("Here's what you have in store for today")
+  fmt.println("I can't predict your day, I'm a computer. But you can tell me")
+}
